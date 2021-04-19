9406c201561ce56c00de63b3f91eee6ed94fe640
Jigsaw <j1g5aw@foxmail.com>
2021-04-20 00:40:04 +0800

    :construction: Complete parser and data

--- nonebot_plugin_transfer/data.py
+++ nonebot_plugin_transfer/data.py
@@ -1,117 +1,109 @@
 import yaml
-from pydantic import BaseModel
 from pathlib import Path
-from typing import Union, Optional, List, Dict
+from typing import Literal, Optional, List, Dict
 
 __DATA_PATH = Path() / "data" / "transfer" / "conv_mapping.yml"
 
-class Map(BaseModel):
-    user: Union[Dict[int,"Map"],List[int]]
-    group: Union[Dict[int,"Map"],List[int]]
-
 def get_conv_mapping(
-    reverse: bool = False,
-) -> Dict[str, Dict[int, Dict[str, List[int]]]]:
+    type: Optional[str] = None,
+    user_id: Optional[int] = None,
+    group_id: Optional[int] = None,
+) -> Dict[str, List[int]]:
+
+    if type is None:
+        type = "user" if user_id else "group"
+    id = user_id if type == "user" else group_id
 
     conv_mapping = __load_conv_mapping()
 
-    if reverse:
-        reverse_conv_mapping = {"user": {}, "group": {}}
-        for send_type in conv_mapping:
-            for send_conv_id in conv_mapping[send_type]:
-                for recv_type in conv_mapping[send_type][send_conv_id]:
-                    for recv_conv_id in conv_mapping[send_type][send_conv_id][
-                        recv_type
-                    ]:
-                        if recv_conv_id in reverse_conv_mapping[recv_type]:
-                            if (
-                                send_type
-                                in reverse_conv_mapping[recv_type][recv_conv_id]
-                            ):
-                                reverse_conv_mapping[recv_type][recv_conv_id][
-                                    send_type
-                                ].append(send_conv_id)
-                            else:
-                                reverse_conv_mapping[recv_type][recv_conv_id][
-                                    send_type
-                                ] = [send_conv_id]
-                        else:
-                            reverse_conv_mapping[recv_type][recv_conv_id] = {
-                                send_type: [send_conv_id]
-                            }
-        return reverse_conv_mapping
+    if id in conv_mapping[type]:
+        tmp_conv_mapping = conv_mapping[type][id]
     else:
-        return conv_mapping
+        tmp_conv_mapping = {"user":[],"group":[]}
 
+    return tmp_conv_mapping
 
 def link_conv(
-    send_type: Optional[str] = None,
-    send_user_id: Optional[int] = None,
-    send_group_id: Optional[int] = None,
-    recv_type: Optional[str] = None,
-    recv_user_id: Optional[int] = None,
-    recv_group_id: Optional[int] = None,
-):
+    user_id_a: List[int] = [],
+    group_id_a: List[int] = [],
+    user_id_b: List[int] = [],
+    group_id_b: List[int] = [],
+) -> Dict[str, ]:
     __update_conv_mapping(
         True,
-        send_type,
-        send_user_id,
-        send_group_id,
-        recv_type,
-        recv_user_id,
-        recv_group_id,
+        user_id_a,
+        group_id_a,
+        user_id_b,
+        group_id_b,
     )
 
 
 def unlink_conv(
-    send_type: Optional[str] = None,
-    send_user_id: Optional[int] = None,
-    send_group_id: Optional[int] = None,
-    recv_type: Optional[str] = None,
-    recv_user_id: Optional[int] = None,
-    recv_group_id: Optional[int] = None,
-):
+    user_id_a: List[int] = [],
+    group_id_a: List[int] = [],
+    user_id_b: List[int] = [],
+    group_id_b: List[int] = [],
+) -> Dict[str, List[int]]:
     __update_conv_mapping(
         False,
-        send_type,
-        send_user_id,
-        send_group_id,
-        recv_type,
-        recv_user_id,
-        recv_group_id,
+        user_id_a,
+        group_id_a,
+        user_id_b,
+        group_id_b,
     )
 
 
 # 更新会话映射
 def __update_conv_mapping(
     link: bool,
-    send_type: Optional[str] = None,
-    send_user_id: Optional[int] = None,
-    send_group_id: Optional[int] = None,
-    recv_type: Optional[str] = None,
-    recv_user_id: Optional[int] = None,
-    recv_group_id: Optional[int] = None,
-):
-    conv_mapping = get_conv_mapping()
+    user_id_a: List[int] = [],
+    group_id_a: List[int] = [],
+    user_id_b: List[int] = [],
+    group_id_b: List[int] = [],
+) -> Dict[str, List[int]]:
 
-    if not send_type:
-        send_type = "user" if send_user_id else "group"
-    send_conv_id = send_user_id if send_type == "user" else send_group_id
+    conv_mapping = __load_conv_mapping()
 
-    if not recv_type:
-        recv_type = "user" if recv_user_id else "group"
-    recv_conv_id = recv_user_id if recv_type == "user" else recv_group_id
+    conv_a = [["user", user_id] for user_id in user_id_a] + [["group", group_id] for group_id in group_id_a]
+    conv_b = [["user", user_id] for user_id in user_id_b] + [["group", group_id] for group_id in group_id_b]
 
     if link:
-        if send_conv_id in conv_mapping[send_type]:
-            if recv_type in conv_mapping[send_type][send_conv_id]:
-                conv_mapping[send_type][send_conv_id][recv_type].append(recv_conv_id)
-            else:
-                conv_mapping[send_type][send_conv_id][recv_type] = [recv_conv_id]
-        else:
-            conv_mapping[send_type][send_conv_id] = {recv_type: [recv_conv_id]}
+        for type_a,id_a in conv_a:
+            for type_b,id_b in conv_b:
+                if id_a not in conv_mapping[type_a]:
+                    conv_mapping[type_a][id_a] = {"user": [id_b], "group": []}
+                else:
+                    if id_b not in conv_mapping[type_a][id_a][type_b]:
+                        conv_mapping[type_a][id_a][type_b].append(id_b)
+                if id_b not in conv_mapping[type_b]:
+                    conv_mapping[type_b][id_b] = {"user": [id_a], "group": []}
+                else:
+                    if id_a not in conv_mapping[type_b][id_b][type_a]:
+                        conv_mapping[type_b][id_b][type_a].append(id_a)
     else:
-        conv_mapping[send_type][send_conv_id][recv_type].remove(recv_conv_id)
+        for type_a,id_a in conv_a:
+            for type_b,id_b in conv_b:
+                if id_a in conv_mapping[type_a]:
+                    if id_b in conv_mapping[type_a][id_a][type_b]:
+                        conv_mapping[type_a][id_a][type_b].remove(id_b)
+                    is_empty = True
+                    for type in conv_mapping[type_a][id_a]:
+                        if conv_mapping[type_a][id_a][type]:
+                            is_empty=False
+                            break
+                    if is_empty:
+                        conv_mapping[type_a].pop(id_a)
+
+                if id_b in conv_mapping[type_b]:
+                    if id_a in conv_mapping[type_b][id_b][type_a]:
+                        conv_mapping[type_b][id_b][type_a].remove(id_a)
+                    is_empty = True
+                    for type in conv_mapping[type_b][id_b]:
+                        if conv_mapping[type_b][id_b][type]:
+                            is_empty=False
+                            break
+                    if is_empty:
+                        conv_mapping[type_b].pop(id_b)
 
     __dump_conv_mapping(conv_mapping)
 
@@ -126,6 +118,7 @@ def __load_conv_mapping() -> Dict[str, Dict[int, Dict[str, List[int]]]]:
 
 # 保存会话映射
 def __dump_conv_mapping(conv_mapping: Dict[str, Dict[int, Dict[str, List[int]]]]):
+
     __DATA_PATH.parent.mkdir(parents=True, exist_ok=True)
     yaml.dump(
         conv_mapping,
--- nonebot_plugin_transfer/parser.py
+++ nonebot_plugin_transfer/parser.py
@@ -7,27 +7,42 @@ transfer_parser = ArgumentParser("trans")
 transfer_subparsers = transfer_parser.add_subparsers()
 
 link_parser = transfer_subparsers.add_parser(
-    "link", help="Link conv_group what you want"
+    "link", help="Link convsations with another what you want"
 )
-link_group = link_parser.add_mutually_exclusive_group()
-link_group.add_argument("-u", "--user", action="store", type=int)
-link_group.add_argument("-g", "--group", action="store", type=int)
+link_parser.add_argument("-ua", "--user-a",action="store", nargs="*", type=int)
+link_parser.add_argument("-ga", "--group-a",action="store", nargs="*", type=int)
+link_parser_group = link_parser.add_argument_group(required=True)
+link_parser_group.add_argument("-u","-ub", "--user-b",action="store", nargs="+", type=int)
+link_parser_group.add_argument("-g","-gb", "--group-b",action="store", nargs="+", type=int)
 link_parser.set_defaults(handle=handle_link)
 
-
 unlink_parser = transfer_subparsers.add_parser(
-    "unlink", help="Unlink current conv_group"
+    "unlink", help="Unlink current conv_parser"
 )
-unlink_group = unlink_parser.add_mutually_exclusive_group()
-unlink_group.add_argument("-u", "--user", action="store", type=int)
-unlink_group.add_argument("-g", "--group", action="store", type=int)
+unlink_parser.add_argument("-ua", "--user-a",action="store", nargs="+", type=int)
+unlink_parser.add_argument("-ga", "--group-a",action="store", nargs="+", type=int)
+unlink_parser.add_argument("-u","-ub", "--user-b",action="store", nargs="+", type=int)
+unlink_parser.add_argument("-g","-gb", "--group-b",action="store", nargs="+", type=int)
 unlink_parser.set_defaults(handle=handle_unlink)
 
-send_parser = transfer_subparsers.add_parser(
-    "send", help="Send message to current conv_group"
+clear_parser = transfer_subparsers.add_parser(
+    "clear", help="Unlink current conv_parser"
 )
-send_group = send_parser.add_mutually_exclusive_group()
-send_group.add_argument("-u", "--user", action="store", type=int)
-send_group.add_argument("-g", "--group", action="store", type=int)
+clear_parser.add_argument("-u", "--user",action="store", nargs="+", type=int)
+clear_parser.add_argument("-g", "--group",action="store", nargs="+", type=int)
+clear_parser.set_defaults(handle=handle_clear)
+
+list_parser = transfer_subparsers.add_parser("list")
+list_parser_group = list_parser.add_mutually_exclusive_group()
+list_parser_group.add_argument("-u", "--user", action="store", type=int)
+list_parser_group.add_argument("-g", "--group", action="store", type=int)
+list_parser.set_defaults(handle=handle_list)
+
+send_parser = transfer_subparsers.add_parser(
+    "send", help="Send message to current conv_parser"
+)
 send_parser.add_argument("message", action="store")
+send_parser.add_argument("-u", "--user", action="store", type=int)
+send_parser.add_argument("-g", "--group", action="store", type=int)
+send_parser.add_argument("-a", "--all", action="store_true")
 send_parser.set_defaults(handle=handle_send)
