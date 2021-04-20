8985fe2af0faa3559268387c8e115c00e2738c99
Jigsaw <j1g5aw@foxmail.com>
2021-04-21 01:42:29 +0800

    :recycle: Refactor data.py (now it's OO)

--- nonebot_plugin_puppet/data.py
+++ nonebot_plugin_puppet/data.py
@@ -1,116 +1,132 @@
 import yaml
 from pathlib import Path
-from typing import Union, Optional, List, Dict
+from typing import List, Dict
 
-__DATA_PATH = Path() / "data" / "puppet" / "conv_mapping.yml"
+Conv = Dict[str, List[int]]
 
 
-def get_conv_mapping(
-    type: Optional[str] = None,
-    user_id: Optional[int] = None,
-    group_id: Optional[int] = None,
-) -> Dict[str, List[int]]:
+class ConvMapping:
+    path: Path
+    conv_mapping: Dict[str, Dict[int, Conv]]
 
-    conv_mapping = __load_conv_mapping()
+    def __init__(self, path: Path = Path() / "data" / "puppet" / "conv_mapping.yml"):
+        self.__load(self, path)
 
-    if type is None:
-        type = "user" if user_id else "group"
-    id = user_id if type == "user" else group_id
+    def get_conv(self, conv: Conv = {"user": [], "group": []}) -> Conv:
 
-    if id in conv_mapping[type]:
-        tmp_conv_mapping = conv_mapping[type][id]
-    else:
         tmp_conv_mapping = {"user": [], "group": []}
 
-    return tmp_conv_mapping
+        if not conv == {"user": [], "group": []}:
+            for type in conv:
+                if conv[type] in self.conv_mapping[type]:
+                    tmp_conv_mapping = self.conv_mapping[type][conv[type]]
+        else:
+            for type in self.conv_mapping:
+                for id in self.conv_mapping[type]:
+                    tmp_conv_mapping[type].append(id)
 
+        return tmp_conv_mapping
 
-def link_conv(
-    conv_a: List[List[Union[str, int]]] = [],
-    conv_b: List[List[Union[str, int]]] = [],
-):
-    return __update_conv_mapping(
-        True,
-        conv_a,
-        conv_b,
-    )
+    def update_conv(self, conv: Conv) -> "ConvMapping":
+        other = ConvMapping().__add_conv(conv).__remove_conv(conv)
+        self.__add_conv(conv).__remove_conv(other.get_conv())
+        self.__dump()
+        return self
 
+    # 添加会话映射
+    def link_conv(
+        self, conv_a: Conv, conv_b: Conv
+    ) -> Dict[str, Dict[int, Dict[str, Dict[int, bool]]]]:
+        result = {"user": {}, "group": {}}
+        for type in result:
+            for id in conv_a[type] + conv_b[type]:
+                result[type][id] = {"user": {}, "group": {}}
+        for type_a in conv_a:
+            for id_a in conv_a[type_a]:
+                for type_b in conv_b:
+                    for id_b in conv_b[type_b]:
+                        if type_a == type_b and conv_a == conv_b:
+                            result[type_a][id_a][type_b][id_b] = False
+                            continue
+                        if id_b in self.conv_mapping[type_a][id_a][type_b]:
+                            result[type_a][id_a][type_b][id_b] = False
+                        else:
+                            self.conv_mapping[type_a][id_a].append(id_b)
+                            result[type_a][id_a][type_b][id_b] = True
+                        if id_a in self.conv_mapping[type_b][id_b][type_a]:
+                            result[type_b][id_b][type_a][id_a] = False
+                        else:
+                            self.conv_mapping[type_b][id_b][type_a].append(id_a)
+                            result[type_b][id_b][type_a][id_a] = True
+        self.__dump()
+        return result
 
-def unlink_conv(
-    conv_a: List[List[Union[str, int]]] = [],
-    conv_b: List[List[Union[str, int]]] = [],
-):
-    return __update_conv_mapping(
-        False,
-        conv_a,
-        conv_b,
-    )
+    # 移除会话映射
+    def unlink_conv(
+        self, conv_a: Conv, conv_b: Conv
+    ) -> Dict[str, Dict[int, Dict[str, Dict[int, bool]]]]:
+        result = {"user": {}, "group": {}}
+        for type in result:
+            for id in conv_a[type] + conv_b[type]:
+                result[type][id] = {"user": {}, "group": {}}
+        for type_a in conv_a:
+            for id_a in conv_a[type_a]:
+                for type_b in conv_b:
+                    for id_b in conv_b[type_b]:
+                        if type_a == type_b and conv_a == conv_b:
+                            result[type_a][id_a][type_b][id_b] = False
+                            continue
+                        if id_b not in self.conv_mapping[type_a][id_a][type_b]:
+                            result[type_a][id_a][type_b][id_b] = False
+                        else:
+                            self.conv_mapping[type_a][id_a][type_b].remove(id_b)
+                            result[type_a][id_a][type_b][id_b] = True
+                        if id_a not in self.conv_mapping[type_b][id_b][type_a]:
+                            result[type_b][id_b][type_a][id_a] = False
+                        else:
+                            self.conv_mapping[type_b][id_b][type_a].remove(id_a)
+                            result[type_b][id_b][type_a][id_a] = True
+        self.__dump()
+        return result
 
+    # 添加会话
+    def __add_conv(self, conv: Conv) -> "ConvMapping":
+        for type in conv:
+            for id in conv[type]:
+                if id not in self.conv_mapping[type]:
+                    self.conv_mapping[type][id] = {"user": [], "group": []}
+        self.__dump()
+        return self
 
-def auto_update_conv_mapping(conv: List[List[Union[str, int]]] = []):
+    # 移除会话
+    def __remove_conv(self, conv: Conv) -> "ConvMapping":
+        new_conv_mapping = {"user": {}, "group": {}}
+        for type_a in self.conv_mapping:
+            for id_a in self.conv_mapping[type_a]:
+                if id_a not in conv[type_a]:
+                    new_conv_mapping[type_a][id_a] = self.conv_mapping[type_a][id_a]
+                    for type_b in self.conv_mapping[type_a][id_a]:
+                        for id_b in self.conv_mapping[type_a][id_a][type_b]:
+                            if id_b in conv[type_b]:
+                                new_conv_mapping[type_a][id_a][type_b].remove(id_b)
+        self.conv_mapping = new_conv_mapping
+        self.__dump()
+        return self
 
-    conv_mapping = __load_conv_mapping()
-    new_conv_mapping = {"user": {}, "group": {}}
+    # 导入会话映射
+    def __load(self, path: Path) -> "ConvMapping":
+        self.path = path
+        try:
+            self.conv_mapping = yaml.safe_load(self.path.open("r", encoding="utf-8"))
+        except FileNotFoundError:
+            self.conv_mapping = {"user": {}, "group": {}}
+        return self
 
-    for type, id in conv:
-        if id not in conv_mapping:
-            new_conv_mapping[type][id] = {"user": [], "group": []}
-
-    for type_a in conv_mapping:
-        for id_a in conv_mapping[type_a]:
-            if id_a in [id for type, id in conv]:
-                new_conv_mapping[type_a][id_a] = conv_mapping[type_a][id_a]
-                for type_b in conv_mapping[type_a][id_a]:
-                    for id_b in conv_mapping[type_a][id_a][type_b]:
-                        if id_b not in [id for type, id in conv]:
-                            new_conv_mapping[type_a][id_a][type_b].remove(id_b)
-
-    __dump_conv_mapping(new_conv_mapping)
-
-
-# 更新会话映射
-def __update_conv_mapping(
-    link: bool,
-    conv_a: List[List[Union[str, int]]] = [],
-    conv_b: List[List[Union[str, int]]] = [],
-):
-
-    conv_mapping = __load_conv_mapping()
-
-    if link:
-        for type_a, id_a in conv_a:
-            for type_b, id_b in conv_b:
-                if type_a == type_b and id_a == id_b:
-                    continue
-                if id_b not in conv_mapping[type_a][id_a][type_b]:
-                    conv_mapping[type_a][id_a][type_b].append(id_b)
-                if id_a not in conv_mapping[type_b][id_b][type_a]:
-                    conv_mapping[type_b][id_b][type_a].append(id_a)
-    else:
-        for type_a, id_a in conv_a:
-            for type_b, id_b in conv_b:
-                if id_b in conv_mapping[type_a][id_a][type_b]:
-                    conv_mapping[type_a][id_a][type_b].remove(id_b)
-                if id_a in conv_mapping[type_b][id_b][type_a]:
-                    conv_mapping[type_b][id_b][type_a].remove(id_a)
-
-    __dump_conv_mapping(conv_mapping)
-
-
-# 保存会话映射
-def __load_conv_mapping() -> Dict[str, Dict[int, Dict[str, List[int]]]]:
-    try:
-        return yaml.safe_load(__DATA_PATH.open("r", encoding="utf-8"))
-    except FileNotFoundError:
-        return {"user": {}, "group": {}}
-
-
-# 保存会话映射
-def __dump_conv_mapping(conv_mapping: Dict[str, Dict[int, Dict[str, List[int]]]]):
-
-    __DATA_PATH.parent.mkdir(parents=True, exist_ok=True)
-    yaml.dump(
-        conv_mapping,
-        __DATA_PATH.open("w", encoding="utf-8"),
-        allow_unicode=True,
-    )
+    # 导出会话映射
+    def __dump(self):
+        self.path.parent.mkdir(parents=True, exist_ok=True)
+        yaml.dump(
+            self.conv_mapping,
+            self.path.open("w", encoding="utf-8"),
+            allow_unicode=True,
+        )
